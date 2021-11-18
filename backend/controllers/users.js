9b6353a7b18b237b62ecffc5287014d1280d15a0
Stepanyan Stepan <Done.Foor@yandex.ru>
2021-11-18 16:48:59 +0300

    .

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -12,18 +12,11 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
+      jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
-        })
-        .send({ token });
     })
     .catch((err) => {
       next(new UnauthorizedError(`Произошла ошибка: ${err.message}`));
